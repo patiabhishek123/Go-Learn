fe8ff5a60548e863dac8c9b31c51952717c8712e
patiabhishek123 <abhishekcloud2003@gmail.com>
2025-12-18 12:45:29 +0530

    learnt about declarations and assignment of variables and various datatypes

---
+++ 03_variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 23
+	fmt.Println(a)
+	var name string = "golang"
+	var name2 = "golang infernce without using var"
+	fmt.Println(name)
+    fmt.Println(name2)
+
+	//shorthand syntax for assigning and declaring variabe simutaneously
+	animal:="tiger"
+	fmt.Println(animal)
+}
