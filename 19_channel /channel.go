942be586ea7b74b051b1ab7238d16208e8d7ee37
patiabhishek123 <abhishekcloud2003@gmail.com>
2026-01-14 00:26:38 +0530

    added a email sender function using goroutines

--- 19_channel /channel.go
+++ 19_channel /channel.go
@@ -11,16 +11,37 @@ for num:= range numchan {
 		fmt.Println("Processing channel -",num)
 	time.Sleep(time.Second)}
 }
-//msin is processing itself in aseperate go routine
+
+func emailSender(emailChan<- chan string,done chan <-bool ){
+	defer func ()  {
+			done<- true
+		}()
+	for email:= range emailChan{
+		fmt.Println("sending email to : ",email)
+		time.Sleep(time.Second)
+		
+	}
+}
+//main is processing itself in aseperate go routine
+
 func main(){
 	//channels are used to comminucate between goroutines
-	numChan :=make(chan int)
-	
-	go processNum(numChan)
-	for{
-		numChan<-rand.Intn(100)
+	// numChan :=make(chan int)
+	// go processNum(numChan)
+	// for{
+	// 	numChan<-rand.Intn(100)
+	// }
+	// // numChan <-5
+
+	emailChan:=make(chan string)
+	done:=make(chan bool)
+	go emailSender(emailChan,done)
+	for i := 0; i < 100; i++ {
+		emailChan<-fmt.Sprint("%d%dabhishek@gmail.com",i,rand.Int31n(50));
 	}
-	// numChan <-5
+	fmt.Println("message is sending ... ")
+
+
 
 
 }
